f39cae5edf0bd4310d0beff7a8f713e3e229ddaf
KNR1997 <kethakaranasinghe@gmail.com>
2025-08-19 14:51:52 +0530

    modified: Subject

--- app/models/subject.py
+++ app/models/subject.py
@@ -3,7 +3,7 @@ from tortoise import fields
 from .base import BaseModel, TimestampMixin
 
 
-class Subject(BaseModel, TimestampMixin):
+class Subject(BaseModel):
     name = fields.CharField(max_length=20, unique=True, index=True)
     code = fields.CharField(max_length=20, unique=True, index=True)
 
